5f44d6f6a84f803fa4a0e5d64c63c610cc5ea542
= <druan10@gmail.com>
2017-02-21 00:41:41 -0800

    added print transition table

--- word-count-dfa/wordcount_dfa.c
+++ word-count-dfa/wordcount_dfa.c
@@ -2,66 +2,90 @@
 #include <math.h>
 #include <stdlib.h>
 
-int alphabet[128];
-int word_length = 0;
-int num_of_unique_characters = 0;
-int num_of_symbols = 0;
-int num_of_states = 0;
-int transition_table[100][128];
+int alphabet[128]; //Characters in the language
+int ascii_values[128]; //Used to hold ascii values of characters in order
+int word_length = 0, num_of_unique_characters = 0, num_of_symbols = 0, num_of_states = 0;
+int transition_table[100][128]; //[state][column]
 char *file_contents;
 long input_file_size;
-/*void print_trans_table()*/
 
-/*Return the length of the inputted word*/
-int get_word_length(char* word) {
+int get_word_length(char* word){
   int i = 0;
-  while (word[i] != '\0') {i++ ;}
+  while (word[i] != '\0') {i++;}
   return (i);
 }
 
 /*Return the number of unique characters found in our alphabet*/
-int get_num_characters() {
-  int count = 0;
-  for (int i = 0; i < 128; i++) {
+int get_num_of_unique_characters() {
+  int i, count = 0;
+  for (i = 0; i < 128; i++) {
     if ( alphabet[i] != 0 ) {count++;}
   }
   return count;
-
 }
 
-/*Build a frequency table for the given word*/
+/*Construct character frequency table for the given word*/
 void construct_frequency_table(char *word) {
-  int i;
-  int count = 0;
-  for (i = 0; i < 128; i++) {alphabet[i] = 0;} //initialize alphabet
-  for (i = 0; i < word_length; i++) { //increment appropriate arrays 
-    if (word[i] < 97) {
-      alphabet[word[i+32]]++;
+  int i, count = 0, ascii_value = 0;
+  
+  for (i = 0; i < 128; i++) {alphabet[i] = 0;}
+
+  for (i = 0; i < word_length; i++){ 
+    ascii_value = (int) word[i];
+    if (ascii_value < 97){
+      alphabet[ascii_value+32]++;
     } else {
-      alphabet[word[i]]++;
+      alphabet[ascii_value]++;
     }
+  }
+
+  // i = 0;
+  // printf("Unique Characters Found: ");
+
+  // for (i = 0; i < 128; i++){
+  //   if ( alphabet[i] != 0){
+  //     printf(" %c", (char) i);
+  //   }
+  // }
+  // printf("\n");
+}
+
+
+void print_trans_table(char *word){
+  //Print Heading
+  printf(" * | _ , .");
+  for (int i = 0; i < word_length; i++) {
+    printf(" %c", (char) ascii_values[i]);
+  }
+  printf("\n");
+  //Print Table
+  for (int i = 0; i < num_of_states; i++) {
+    printf(" %i |",i); //Print State
+    for (int j = 0; j < num_of_symbols; j++) {
+      printf(" %i", transition_table[i][j]); //Print value @ index [i][j], the position to move to
+    }
+  printf("\n");
   }
 }
 
 void construct_transition_table(char *word) {
-  word_length = get_word_length(word);
   //Calculate the properties for the transition table
   construct_frequency_table(word);
-  num_of_symbols = get_num_characters() + 3; //include space, comma, period
-  num_of_states = word_length + 3; //1 step for every letter, 1 for accept, 1 for fail, and 1 for init
+  num_of_symbols = get_num_of_unique_characters() + 3; 
+  num_of_states = word_length + 4; 
 
-  printf("Number of Symbols: %i\n", num_of_symbols);
-  printf("Number of States: %i\n", num_of_states);
+  int count = 0;
 
-}
-
-void print_trans_table(){
-  for (int i = 0; i < num_of_symbols; i++) {
-    for (int j = 0; i < num_of_states; j++) {
-      printf("%i ", transition_table[i][j]);
+  for (int i = 0; i < 128; i++){
+    if ( alphabet[i] != 0 ) {
+      ascii_values[count] = i;
+      count++;
     }
-  printf("\n");
   }
+
+  printf("Number of Symbols in transition table: %i\n", num_of_symbols);
+  printf("Number of States: %i\n", num_of_states);
+  print_trans_table(word);
 }
 
 int main(int argc, char *argv[])
@@ -99,15 +123,12 @@ int main(int argc, char *argv[])
   printf("\n") ;
 
   /////////////////////////////////////////////////////////////////////////
-  /*Creates a transition table and parses the string
+  
   word_length = get_word_length(word);
   printf("Word Length: %i\n", word_length);
-  construct_frequency_table(word);
-  int symbols[num_of_unique_characters+3]; //Unique characters + space, comma, and Period
-  printf("Number of Unique Characters in \'%s\': %i\n", word, num_of_unique_characters);
-  */
   construct_transition_table(word);
-
+  num_of_unique_characters=get_num_of_unique_characters();
+  printf("Number of Unique Characters in \'%s\': %i\n", word, num_of_unique_characters);
   return 0;
 
 }
